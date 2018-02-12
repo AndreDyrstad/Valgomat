7c55792ac7ae226ded490f4b2e67e50dd19c2aac
Andre Dyrstad <dyrstad.a@gmail.com>
2018-02-12 12:40:53 +0100

    Fixed checkboxes

--- src/app/issues2/issues2.component.ts
+++ src/app/issues2/issues2.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Issues2Component implements OnInit {
 
+  title = 'Tilbud ved problemstillinger knyttet til';
   constructor() { }
 
   ngOnInit() {
