7cf22617e80668b2c5018fb4f8cbe03a3e7c08f6
Andre Dyrstad <dyrstad.a@gmail.com>
2018-02-27 16:39:54 +0100

    Added bluemix server API instead of loacalhost

--- src/app/http-service.ts
+++ src/app/http-service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
 export class HttpService{
   constructor(private _http: HttpClient) {}
   getData() {
-    return this._http.get('https://valgomatserver-turbulent-sable.eu-gb.mybluemix.net/center')
+    return this._http.get('https://valgomatserver.eu-gb.mybluemix.net/center')
       .map(res => res);
   }
   sendData() {
@@ -16,10 +16,10 @@ export class HttpService{
       'Content-Type': 'application/x-www-form-urlencoded'
     });
 
-    return this._http.post('https://valgomatserver-turbulent-sable.eu-gb.mybluemix.net/center', json).map(res => res);
+    return this._http.post('https://valgomatserver.eu-gb.mybluemix.net/center', json).map(res => res);
   }
   sendIssues2(json) {
-    return this._http.post('https://valgomatserver-turbulent-sable.eu-gb.mybluemix.net/center', json).map(res => res);
+    return this._http.post('https://valgomatserver.eu-gb.mybluemix.net/center', json).map(res => res);
   }
   getLocalIssue() {
     return this._http.get('http://localhost:4200/assets/issues2.json').map(res => res);
