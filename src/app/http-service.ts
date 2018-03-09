c7a2d2d59993e00ce05704d1f30a2419726c3d3c
Andre Dyrstad <dyrstad.a@gmail.com>
2018-03-09 13:12:12 +0100

    Looks a bit better

--- src/app/http-service.ts
+++ src/app/http-service.ts
@@ -7,8 +7,8 @@ export class HttpService{
   constructor(private _http: HttpClient) {}
 
   sendIssues2(json) {
-    return this._http.post('https://valgomatserver.eu-gb.mybluemix.net/api/center', json);
-   // return this._http.post('http://localhost:3000/center', json).map(res => res);
+   // return this._http.post('https://valgomatserver.eu-gb.mybluemix.net/api/center', json);
+    return this._http.post('http://localhost:3000/center', json);
   }
   getLocalIssue2() {
     return this._http.get('http://localhost:4200/assets/issues2.json');
