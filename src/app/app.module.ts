e7ed9d1b08a1c2cb8231d486c50e0bcfc6a44a2b
Andre Dyrstad <dyrstad.a@gmail.com>
2018-03-19 13:27:15 +0100

    More question fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {OffersComponent} from './offers/offers.component';
-import { OfferMsComponent } from './offer-ms/offer-ms.component';
-import { IssueComponent } from './issue/issue.component';
-import { YesNoComponent } from './yes-no/yes-no.component';
-import { Issues2Component } from './issues2/issues2.component';
 import {FormsModule} from '@angular/forms';
 import {HttpService} from './http-service';
 import {HttpClientModule} from '@angular/common/http';
@@ -16,11 +11,6 @@ import { CenterComponent } from './center/center.component';
 @NgModule({
   declarations: [
     AppComponent,
-    OffersComponent,
-    OfferMsComponent,
-    IssueComponent,
-    YesNoComponent,
-    Issues2Component,
     CenterComponent,
   ],
   imports: [
